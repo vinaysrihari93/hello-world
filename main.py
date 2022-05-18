e0925c50f7e536187fb823d8151c56f8820b3099
vinaysrihari93 <vinaysrihari93@gmail.com>
2022-05-18 20:52:17 +0530

    'Hi_user123'

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.fvhxsafhgxsfhagfgdhtcysffcghgchgscd
 if __name__ == '__main__':
-    print_hi('PyCharm Test')
+    print_hi('PyCharm Test Branch')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
